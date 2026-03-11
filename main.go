90f8302a23cbfe4ea9e3783eda009c5b146e444b
rh <rhruban@gmail.com>
2026-03-10 22:20:35 -0600

    added handler for readiness endpoint

--- main.go
+++ main.go
@@ -10,7 +10,12 @@ func main() {
 	const port = "8080"
 	
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	srv := &http.Server{
 		Addr: ":" + port,
@@ -20,3 +25,5 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
+
+
