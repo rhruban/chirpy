735338854df10273c0bd94752a0064f84c773c13
rh <rhruban@gmail.com>
2026-03-24 10:16:32 -0600

    added database

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,5 +1,10 @@
 -- +goose up
-CREATE TABLE ...
+CREATE TABLE users (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  email TEXT NOT NULL UNIQUE
+);
 
 -- +goose down
 DROP TABLE users;
